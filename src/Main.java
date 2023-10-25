8b1b8cba27a6f7d927027a0166dfbfdf0e031d2f
EBerk26 <eli_berk26@milton.edu>
2023-10-25 10:08:25 -0400

    Creates and describes 10 random ponies.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,43 @@
 public class Main {
-    public static void main(String[] args) {
-        Ponies p = new Ponies(false,1);
+    public static int RandInt(int LowerBound, int UpperBound){
+        return(int)(Math.random()*(UpperBound-LowerBound+1)+LowerBound);
+    }
+    public static void describeRandomPony(int ponyNumber){
+        Ponies RandomPony= new Ponies();
+        RandomPony.hasHorn = Math.random()<=0.4;
+        RandomPony.hasWings = Math.random()<=0.25;
+        RandomPony.hornLength = (int)(Math.random()*9+1);
 
+        String[] NameLibrary = new String[10];
+        NameLibrary[0] = "Eli";
+        NameLibrary[1] = "Teddy";
+        NameLibrary[2] = "Gabe";
+        NameLibrary[3] = "Ava";
+        NameLibrary[4] = "Mx. Bradford";
+        NameLibrary[5] = "Finn";
+        NameLibrary[6] = "Pati";
+        NameLibrary[7] = "Rae";
+        NameLibrary[8] = "Claire";
+        NameLibrary[9] = "Sparkles";
+
+        RandomPony.name = NameLibrary[RandInt(0,9)];
+       String UnicornDescription;
+        if(RandomPony.hasHorn){
+            UnicornDescription = ("is a unicorn and has a horn length of "+ RandomPony.hornLength+" inches");
+        } else {
+            UnicornDescription = "is not a unicorn";
+        }
+        String PegasusDescription;
+        if(RandomPony.hasWings){
+            PegasusDescription = "is a pegasus";
+        } else {
+            PegasusDescription = "is not a pegasus";
+        }
+        System.out.println("#"+ponyNumber+": "+RandomPony.name+" the pony "+UnicornDescription+". "+RandomPony.name+" "+PegasusDescription+".");
+    }
+    public static void main(String[] args) {
+        for(int x =1; x<=10;x++) {
+            describeRandomPony(x);
+        }
     }
 }
--- src/Ponies.java
+++ src/Ponies.java
@@ -1,18 +1,6 @@
 public class Ponies {
-    public Ponies(boolean hasHorn, int ponyNumber){
-        if(hasHorn) {
-            System.out.println("Pony #" + ponyNumber + " is a unicorn");
-        } else {
-            System.out.println("Pony #" + ponyNumber + " is not a unicorn");
-        }
+        boolean hasHorn;
+        double hornLength;
+        boolean hasWings;
+        String name;
     }
-    public static void main(String[] args) {
-        for(int x = 1;x<=100;x++) {
-            if (Math.random() >=0.5){
-                new Ponies(true,x);
-            } else {
-                new Ponies(false,x);
-            }
-        }
-    }
-}
