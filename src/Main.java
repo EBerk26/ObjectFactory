0580f5e25cc30f9a57ac3524079d563184cf2ae7
EBerk26 <eli_berk26@milton.edu>
2023-10-31 13:09:47 -0400

    Create a describePony method withing Ponies.java - this method is now called by Main.java after generating the RandomPony.

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         RandomPony.hasHorn = Math.random()<=0.4;
         RandomPony.hasWings = Math.random()<=0.25;
         RandomPony.hornLength = (double)(RandInt(1,100))/10;
-        String[] NameLibrary = new String[11];
+        String[] NameLibrary = new String[14];
         NameLibrary[0] = "Eli";
         NameLibrary[1] = "Teddy";
         NameLibrary[2] = "Gabe";
@@ -19,26 +19,15 @@ public class Main {
         NameLibrary[8] = "Claire";
         NameLibrary[9] = "Sparkles";
         NameLibrary[10] = "Fluffy";
-        RandomPony.name = NameLibrary[RandInt(0,10)];
-        String UnicornDescription;
-        if(RandomPony.hasHorn){
-            UnicornDescription = ("is a unicorn and has a horn length of "+ RandomPony.hornLength+" inches");
-        } else {
-            UnicornDescription = "is not a unicorn";
-        }
-        String PegasusDescription;
-        if(RandomPony.hasWings){
-            PegasusDescription = "is a pegasus";
-        } else {
-            PegasusDescription = "is not a pegasus";
-        }
-        System.out.println("#" + ponyNumber + ": " + RandomPony.name + " the pony " + UnicornDescription + ". " + RandomPony.name + " "+PegasusDescription + ".");
-        if(RandomPony.hasHorn && RandomPony.hasWings && RandomPony.hornLength == 10){
-            System.out.println(RandomPony.name + " is a perfect pony!");
-        }
+        NameLibrary[11] = "Sunshine";
+        NameLibrary[12] = "Peanut";
+        NameLibrary[13] = "Daisy";
+        RandomPony.name = NameLibrary[RandInt(0,13)];
+        RandomPony.Number = ponyNumber;
+        RandomPony.describePony();
     }
     public static void main(String[] args) {
-        for(int x =1; x<=1000;x++) {
+        for(int x =1; x<=10;x++) {
             describeRandomPony(x);
         }
     }
--- src/Ponies.java
+++ src/Ponies.java
@@ -1,6 +1,25 @@
 public class Ponies {
-        boolean hasHorn;
-        double hornLength;
-        boolean hasWings;
-        String name;
+        boolean hasHorn = false;
+        double hornLength = 0;
+        boolean hasWings = false;
+        String name = "";
+        int Number = 1;
+        public void describePony(){
+                String UnicornDescription;
+                if(hasHorn){
+                        UnicornDescription = ("is a unicorn and has a horn length of "+ hornLength+" inches");
+                } else {
+                        UnicornDescription = "is not a unicorn";
+                }
+                String PegasusDescription;
+                if(hasWings){
+                        PegasusDescription = "is a pegasus";
+                } else {
+                        PegasusDescription = "is not a pegasus";
+                }
+                System.out.println("#" + Number + ": " + name + " the pony " + UnicornDescription + ". " + name + " "+PegasusDescription + ".");
+                if(hasHorn && hasWings && hornLength == 10){
+                        System.out.println(name + " is a perfect pony!");
+                }
+        }
 }
